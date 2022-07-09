a87dd95bd35c65fed9243cd075b3126e42b1b922
vishnu <vishnurajendran3495@gmail.com>
2022-07-09 21:02:10 +0530

    modify

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,5 +15,4 @@ urlpatterns=[
     path('books',views.books,name="books"),
     path('dictionary',views.dictionary,name="dictionary"),
     path('wiki',views.wiki,name="wiki"),
-    # path('conversion',views.conversion,name="conversion"),
 ]
